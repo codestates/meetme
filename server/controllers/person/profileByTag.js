5a8cd6d92359bc016dd09c02f0a0cf05d73075d4
wolfdale <wolfdale@outlook.kr>
2021-10-09 17:27:26 +0900

    edit sequelize

--- server/controllers/person/profileByTag.js
+++ server/controllers/person/profileByTag.js
@@ -1,9 +1,35 @@
 const db = require('../../../models');
 
 module.exports = async (req, res) => {
-  const _tag = await db.tag.findAll({
-    where: { id: tagId }
+  const _user = await db.user.findAll({
+    include: [
+      {
+        model: db.user_category,
+        required: true,
+        include: [
+          {
+            model: db.category,
+            required: true,
+            include: [
+              {
+                model: db.category_tag,
+                required: true,
+                include: [
+                  {
+                    model: db.tag,
+                    required: true,
+                    where: {
+                      id: req.tagId
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
   });
 
-  res.status(200).json({ users: user });
+  res.status(200).json({ users: _user });
 };
