f4b9a27c265a56d93cdd3e2ba43e9f9acefbb782
wolfdale <wolfdale@outlook.kr>
2021-10-10 19:17:21 +0900

    Edit typo and trimmed some code

--- server/controllers/photo/upload.js
+++ server/controllers/photo/upload.js
@@ -9,12 +9,15 @@ module.exports = async (req, res) => {
   const sql = `update user set = ${db.escape(filepath)}
                where email = '${db.escape(email)}'`;
   await db.query(sql);
+  db.terminate();
 
   const sql2 = `select * from user
                 where email = '${db.escape(email)}'`;
+  const user = await db.query(sql2);
+  db.terminate();
 
   res.status(200).json({
-    email: sql2[0].email,
-    filepath: sql2[0].profile_image
+    email: user[0].email,
+    filepath: user[0].profile_image
   });
 };
