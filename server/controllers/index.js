13a10cd2c57f7d889814bd88c643fe7d42a3f06d
wolfdale <83825051+wolf-dale@users.noreply.github.com>
2021-10-12 15:40:56 +0900

    Delete index.js

--- server/controllers/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    deleteImage: require('./photo/deleteImage'),
-    upload: require('./photo/upload'),
-    close: require('./user/close'),
-    userInfo: require('./user/userInfo')
-}
