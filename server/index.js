fd9716dfb75b1c1c086029b81c4c71222d1b1c3f
wolfdale <wolfdale@outlook.kr>
2021-10-12 19:25:38 +0900

    Edit router

--- server/index.js
+++ server/index.js
@@ -1,38 +1,27 @@
-require('dotenv').config();
-const http = require('http');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-const userRouter = require('./routers/UserRouter');
-const personRouter = require('./routers/PersonRouter');
-const photoRouter = require('./routers/PhotoRouter');
+const { profile, profileByTag } = require('./controllers/person/profile');
+const controllers = require('./controllers');
 
 const express = require('express');
 const app = express();
-
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: false }));
-
-app.use(
-    cors({
-        origin: true,
-        credentials: true,
-        methods: ['GET', 'POST', 'DELETE', 'OPTIONS']
-    })
+@@ -22,9 +21,12 @@ app.use(
 )
 app.use(cookieParser());
 
-app.use('/user', userRouter);
-app.use('/person', personRouter);
-app.use('/photo', photoRouter);
+app.get('/user/profile', profile);
+app.get('/user/profile/:tagName', profileByTag);
+app.post('/photo/deleteimage', controllers.deleteImage);
+app.post('/photo/upload', controllers.upload);
+app.delete('/user/close', controllers.close);
+app.get('/user/userinfo', controllers.userInfo);
 
 // server test code
 // app.get('/', (req, res) => {
 //     res.status(201).send('hello world.')
 // })
-
 const port = 4000;
-
 http.createServer(app).listen(port, () => {
     console.log(`서버가 ${port}에서 정상적으로 작동하고 있습니다.!!`)
 });
